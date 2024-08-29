4c5bdc399ff90c0a352a5810a94d7eb85ec48500
chandruthangarasu <146078817+chandruthangarasu@users.noreply.github.com>
2024-08-29 11:57:14 +0530

    Add files via upload

---
+++ Project18/webprj/WebprjApplication.java
@@ -0,0 +1,13 @@
+package com.stud.webprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebprjApplication.class, args);
+	}
+
+}
